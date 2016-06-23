ea9f5579489b9ab1b27ee09b44513fa4c947a01f
DasonCheng <dasoncheng@foxmail.com>
2016-06-23 11:45:23 +0800

    header-min

--- origin/js/header.js
+++ origin/js/header.js
@@ -4,31 +4,33 @@
 $(function () {
     var list_index = $("header .nav-list ul li.active").index();
     var origin_index = list_index;
+    var nav_list_array = $("header .nav-list ul li");
+    var nav_info_array = $("header .nav-info ul li");
 
-    $("header .nav-list ul li").hover(function () {
+    nav_list_array.hover(function () {
         list_index = $(this).index();
 
-        $("header .nav-list ul li").removeClass("active");
+        nav_list_array.removeClass("active");
         $(this).addClass("active");
 
-        $("header .nav-info ul li").eq(list_index).addClass("active").animate({opacity: 1}, 300).siblings().removeClass("active");
+        nav_info_array.eq(list_index).addClass("active").animate({opacity: 1}, 300).siblings().removeClass("active");
     }, function () {
-        $("header .nav-list ul li").removeClass("active");
-        $("header .nav-list ul li").eq(origin_index).addClass("active");
-        $("header .nav-info ul li").animate({opacity: 0}, 0);
-        $("header .nav-info ul li").eq(origin_index).addClass("active").animate({opacity: 1}, 300).siblings().removeClass("active");
+        nav_list_array.removeClass("active");
+        nav_list_array.eq(origin_index).addClass("active");
+        nav_info_array.animate({opacity: 0}, 0);
+        nav_info_array.eq(origin_index).addClass("active").animate({opacity: 1}, 300).siblings().removeClass("active");
     });
 
 
     $("header .nav-info").hover(function () {
-        $("header .nav-list ul li").removeClass("active");
-        $("header .nav-list ul li").eq(list_index).addClass("active");
-        $("header .nav-info ul li").eq(list_index).addClass("active").animate({opacity: 1}, 0).siblings().removeClass("active");
+        nav_list_array.removeClass("active");
+        nav_list_array.eq(list_index).addClass("active");
+        nav_info_array.eq(list_index).addClass("active").animate({opacity: 1}, 0).siblings().removeClass("active");
 
     }, function () {
-        $("header .nav-list ul li").removeClass("active");
-        $("header .nav-list ul li").eq(origin_index).addClass("active");
-        $("header .nav-info ul li").eq(origin_index).addClass("active").siblings().removeClass("active");
-        list_index=origin_index;
+        nav_list_array.removeClass("active");
+        nav_list_array.eq(origin_index).addClass("active");
+        nav_info_array.eq(origin_index).addClass("active").siblings().removeClass("active");
+        list_index = origin_index;
     })
 });
